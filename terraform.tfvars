c8adfc9db97a8b9b537729f11f0e470cb855f2cf
Tripathy <sumtripathy@deloitte.com>
2017-03-12 20:11:02 +0530

    First commit

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+region = "eu-east-1"
+#aws_access_key = ""
+#aws_secret_key = ""
+vpc_cidr          = "10.2.0.0/16"
+private_subnets    = "10.2.1.0/24,10.2.2.0/24"
+public_subnets    = "10.2.3.0/24,10.2.4.0/24"
+azs               = "eu-east-1a,eu-east-1b,eu-east-1c"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+#AWS VARIABLES
+variable "region" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+##########################################
+# VPC VARIABLES
+variable "vpc_cidr" {}
+variable "private_subnets" {}
+variable "public_subnets" {}
+variable "azs" {}
